1b3ccb0b08349b1c474cae4796098d8d8dbfa013
coderbux <programmeroybek211@gmail.com>
2025-01-09 10:34:30 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,30 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://rolling-sushi.uz" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=600, stale-while-revalidate=600", // 10 daqiqa soat
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);

b9d52b04886905da9fa0fc030e246e2e5b20671a
fikserr <azizxon7777777@gmail.com>
2025-01-20 12:19:27 +0500

    test branch

---
+++ app/[locale]/[place]/about-us/contact/page.jsx
@@ -0,0 +1,15 @@
+import Container from '@/components/shared/container'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <Container>
+      
+
+
+        
+    </Container>
+  )
+}
+
+export default Contact

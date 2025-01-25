46de84392e1e48548f63662d024d3d7f76d54582
Oybek <programmeroybek211@gmail.com>
2025-01-25 12:38:55 +0500

    revalidate banner

--- service/index.js
+++ service/index.js
@@ -2,6 +2,7 @@ import { url } from "@/lib/utils";
 
 const getData = async (endpoint) => {
   const res = await fetch(`${url}${endpoint}`, {
+    cache: "force-cache",
     next: { revalidate: 60 }, // Revalidate every 1 minute (60 seconds)
   });
   const data = await res.json();

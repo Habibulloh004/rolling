344c2cda6c0db285101f8fefcbc3e7cf55b6e318
Habibulloh004 <habibulloh4080@gmail.com>
2025-03-26 17:06:49 +0500

    add reviews

--- app/[locale]/[place]/reviews/page.jsx
+++ app/[locale]/[place]/reviews/page.jsx
@@ -159,6 +159,241 @@ const Reviews = async ({ params }) => {
       text: `Это было оооочень” вкусно`,
       time: "19.01.2025",
     },
+    {
+      id: 16,
+      author_name: "Дамир Муратов",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocJVySIbpSKCIvB3_VHmtxoovynveCzMWtjfTF9rPCey8bWywA=w75-h75-p-rp-mo-br100",
+      text: `Каждый раз заказываю только у вас, очень вкусные роллы,и доставка очень быстро и цены не дорогие рекомендую 👍`,
+      time: "10.01.2025",
+    },
+    {
+      id: 17,
+      author_name: "Shavkat Abdullaev",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocIBg0xXv6Zm83NCqzKv4WT5SWwsTD46HtxxYR3Hw1-14vl86w=w75-h75-p-rp-mo-br100",
+      text: `Самые вкусные роллы в ташкенте, рекомендую💯`,
+      time: "17.01.2025",
+    },
+    {
+      id: 18,
+      author_name: "Озодбек Мирзабаев",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocII5TVOcUOSG9gCQSpbNoEZes0YxLGfQQrhVNYRH6krO1rWIw=w75-h75-p-rp-mo-br100",
+      text: `Вкусные роллы и сытные. 👍`,
+      time: "19.01.2025",
+    },
+    {
+      id: 19,
+      author_name: "Юсуфбек Матниязов",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjX8yO1xGaY1GnHCKq-VhWzWRzf5V4r0ES8T93mGk3H9F9uPpmc=w75-h75-p-rp-mo-br100",
+      text: `Rolling sushi best of the best 👍🔥✊`,
+      time: "27.11.2024",
+    },
+    {
+      id: 20,
+      author_name: "shohjahon davronov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocKu0DnJo33qxGiGkGUohq61z2bdG_UYKr_Ej0Wbrlh4incl7w=w75-h75-p-rp-mo-br100",
+      text: `Лучший суши`,
+      time: "03.07.2024",
+    },
+    {
+      id: 21,
+      author_name: "Shokhzeb Otakhanov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocLCDif_i1LJLnHtgdvJuSFj8UfDkSSqLqmv3DwPkJS5E3LEvg=w75-h75-p-rp-mo-br100",
+      text: `super`,
+      time: "13.07.2024",
+    },
+    {
+      id: 22,
+      author_name: "Firdavs Dadakhonov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjWJaDBO6BdjANhNZHlsbOz4WyFO1CS51mtxHSkp_b5QsQZKbBk-=w75-h75-p-rp-mo-br100",
+      text: `Мне больше нравились роли запичони`,
+      time: "17.01.2025",
+    },
+    {
+      id: 23,
+      author_name: "Zuhriddin Timirov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocKQW41lgssuOcqkg82ncXR8FU1IBIaS7bpgs_f08prcvkBf5zU=w75-h75-p-rp-mo-br100",
+      text: `Это вкусно`,
+      time: "19.07.2024",
+    },
+    {
+      id: 24,
+      author_name: "Dilnoza Dogman",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocKRoz8y4Ukvd7WN2GjpVJWwDUt2jPLBKvegQqtbqmxXtjF5jg=w75-h75-p-rp-mo-br100",
+      text: `Атмосфера очень уютно, суши вкусней чем другие суши, советую не пожалейте😋`,
+      time: "14.01.2025",
+    },
+    {
+      id: 25,
+      author_name: "Ulug'bek Turdiyev",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocKRoz8y4Ukvd7WN2GjpVJWwDUt2jPLBKvegQqtbqmxXtjF5jg=w75-h75-p-rp-mo-br100",
+      text: `Gap yoo`,
+      time: "07.08.2024",
+    },
+    {
+      id: 26,
+      author_name: "Юсуфбек Матниязов",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjX8yO1xGaY1GnHCKq-VhWzWRzf5V4r0ES8T93mGk3H9F9uPpmc=w75-h75-p-rp-mo-br100",
+      text: `Все просто на вышем уровне`,
+      time: "29.11.2024",
+    },
+    {
+      id: 27,
+      author_name: "Дониёрбек Утаров",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjW8erKTj3w1OGZfNaRlchA-Own7TGQgOcUY0SKYG6Ndn3YNQjPI=w75-h75-p-rp-mo-br100",
+      text: `Nice place 🤩`,
+      time: "07.07.2024",
+    },
+    {
+      id: 27,
+      author_name: "Google User",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocJzBwRR1FmlK-OJ-4vGVKrIYgcqmPVmvwwnGoLRoS2cvNKkug=w75-h75-p-rp-mo-ba3-br100",
+      text: `Так как суши сложные на вкус, приготовили быстро, атмосфера расслабляющая, рекомендую`,
+      time: "17.11.2024",
+    },
+    {
+      id: 28,
+      author_name: "Abu Shukurov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjWvDHXjlDkyD31qeOCBTiDBRjtXLBlhs4HBgwcrNyfan7dIrxY=w75-h75-p-rp-mo-br100",
+      text: `Недавно заказал суши в Rolling Sushi, и это был потрясающий опыт! Все блюда были очень свежими, аккуратно приготовленными и доставлены вовремя. Особенно понравился фирменный сет – его вкус и качество просто на высоте. Приятно, что у ресторана удобное мобильное приложение, где можно легко оформить заказ и отслеживать доставку.
+
+      Также хочу отметить вежливость и профессионализм курьеров – всегда приветливые и пунктуальные. Спасибо Rolling Sushi за отличный сервис! Обязательно закажу у вас снова!`,
+      time: "15.01.2025",
+    },
+    {
+      id: 29,
+      author_name: "Азизбек Арзибаев",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocLx4bLsGfpQ_Rzu-4NqDOwU0jc1F7cQqTISN-A2FyXE-LYH9Q=w75-h75-p-rp-mo-br100",
+      text: `Вот это настоящее суши!`,
+      time: "10.12.2024",
+    },
+    {
+      id: 30,
+      author_name: "Alisher Akromov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjVwh9Q9sGyPmunGXRcIfifIMbZBQbHc5U1z5SqIk8vQGdNt7D95=w75-h75-p-rp-mo-br100",
+      text: `Самые лучшие суши только в роллинг суши просто бомбически вкус. И отмосфера просто шикарное`,
+      time: "03.01.2025",
+    },
+    {
+      id: 31,
+      author_name: "Primex Services",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocLGILq5XA9xKaMcEkLOJdHbcXmAPlc-8mfzCMydOcFuB7IuYQ=w75-h75-p-rp-mo-br100",
+      text: `Оочень вкусно🔥 лучше чем во многих знаменитых суши заведений в Ташкенте, а самое главное это недорого 😍`,
+      time: "21.07.2024",
+    },
+    {
+      id: 32,
+      author_name: "Санжар Ёрбеков",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjW16b6fOlyqxNFlEn6qBLTfYh0lFdYGGUGptN1iwHeOulggC08=w75-h75-p-rp-mo-br100",
+      text: `Замечательные суши просто восхитительные.`,
+      time: "28.02.2025",
+    },
+    {
+      id: 33,
+      author_name: "Javlon Tolipov",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjWzLPuetbJfZ0olKQ6QfTIZaxO4Pg3cFDx-GidZnpqcss2Rsmjh=w75-h75-p-rp-mo-br100",
+      text: `Очень вкусно готовят`,
+      time: "17.11.2024",
+    },
+    {
+      id: 34,
+      author_name: "R7express Accounting",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocLke9xwIVz4AVeIs4ba77rBqHNbSG0uDGfAhQFWEonNAY7VzA=w75-h75-p-rp-mo-br100",
+      text: `N1 sushi in Tashkent!👌👌👌`,
+      time: "01.08.2024",
+    },
+    {
+      id: 35,
+      author_name: "Мухаммад Жалолиддинович",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocIX2PhJHYF8zCl8wyJ57ong-cMJLpZ_FCkHxJPmRNRAzzVa1w=w75-h75-p-rp-mo-br100",
+      text: `👍👍👍👍`,
+      time: "30.07.2024",
+    },
+    {
+      id: 36,
+      author_name: "Муборак Ёрбекова",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjV41078RnjFFI13v9VZWJpB-oV7g3vwkB2J96O8R5lDu4Ee7c8=w75-h75-p-rp-mo-br100",
+      text: `Роль очень хорошая, заказ прибыл вовремя, вкус хороший.`,
+      time: "23.02.2025",
+    },
+    {
+      id: 37,
+      author_name: "Habiba Usmonova",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a/ACg8ocKSYqDC9wQ2PCn5O1AoxtZmOfoU41al5GPQEJVgNvh7GOffxg=w75-h75-p-rp-mo-br100",
+      text: `Сервис уже на подходе, не пропустите ♥️`,
+      time: "20.12.2024",
+    },
+    {
+      id: 38,
+      author_name: "John",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjWfCTlXKSjW6dKxvAm_9OXw8g4V4MP0OttBa8SDcZUJZWKNfRKm=w75-h75-p-rp-mo-br100",
+      text: `Простые бомбические суши стоят того, чтобы сходить туда.`,
+      time: "03.07.2024",
+    },
+    {
+      id: 39,
+      author_name: "Отабек Саттаров",
+      rating: 5,
+      profile_photo_url:
+        "https://lh3.googleusercontent.com/a-/ALV-UjVZZATAoaL4RCULIfeaIGRQi_GBzYmqI7M5hcv0gLZj4mstqlcI=w75-h75-p-rp-mo-br100",
+      text: `👍👍👍 Не волнуйтесь `,
+      time: "05.01.2024",
+    },
+    {
+      id: 40,
+      author_name: "",
+      rating: 5,
+      profile_photo_url: "",
+      text: ``,
+      time: "17.11.2024",
+    },
   ];
 
   return (
@@ -184,21 +419,23 @@ const Reviews = async ({ params }) => {
         }}
       >
         <CarouselContent className="relative">
-          {reviewData?.map((item, i) => {
-            if (item.rating < 4) return;
-            return (
-              <CarouselItem
-                key={i}
-                className={`basis-[80%] sm:basis-[45%] lg:basis-[45%] xl:basis-[30%] p-0 mx-2 ${
-                  i == 0 && "max-sm:ml-8 max-md:ml-16 ml-8"
-                }`}
-              >
-                <a href="https://g.co/kgs/YJy7TYy" target="_blank">
-                  <Cards data={item} />
-                </a>
-              </CarouselItem>
-            );
-          })}
+          {[...reviewData]
+            .sort(() => Math.random() - 0.5) // Tasodifiy tartiblash
+            .map((item, i) => {
+              if (item.rating < 4) return;
+              return (
+                <CarouselItem
+                  key={i}
+                  className={`basis-[80%] sm:basis-[45%] lg:basis-[45%] xl:basis-[30%] p-0 mx-2 ${
+                    i == 0 && "max-sm:ml-8 max-md:ml-16 ml-8"
+                  }`}
+                >
+                  <a href="https://g.co/kgs/YJy7TYy" target="_blank">
+                    <Cards data={item} />
+                  </a>
+                </CarouselItem>
+              );
+            })}
         </CarouselContent>
       </Carousel>
     </Container>

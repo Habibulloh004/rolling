13a72970aafa311150d95d1899eff47c45c89e5b
OybekDeveloper <Programmeroybek211@gmail.com>
2025-05-16 23:53:48 +0500

    dfasd

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -168,7 +168,7 @@ export default function Header({
         currentMinutes < openedMinutes && currentMinutes >= closedMinutes;
     }
 
-    setIsDisabled(isDisabled);
+    setIsDisabled(true);
   }, [apiTime]);
 
   useEffect(() => {

9fe866b96be9afcda047228e005adf95b8430c55
OybekDeveloper <Programmeroybek211@gmail.com>
2025-07-31 00:11:53 +0500

    add icons

--- app/[locale]/[place]/icons/page.jsx
+++ app/[locale]/[place]/icons/page.jsx
@@ -7,6 +7,7 @@ export default function IconsPage() {
       <Image src="/icons/1.png" alt="Arrow Left" width={48} height={48} />
       <Image src="/icons/2.png" alt="Arrow Left" width={48} height={48} />
       <Image src="/icons/3.png" alt="Arrow Left" width={48} height={48} />
+      icons
     </div>
   )
 }

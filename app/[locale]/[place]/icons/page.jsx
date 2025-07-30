2672b64d8b87cde397778f6518e26517d1461be2
OybekDeveloper <Programmeroybek211@gmail.com>
2025-07-31 00:06:40 +0500

    add icons

---
+++ app/[locale]/[place]/icons/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function IconsPage() {
+  return (
+    <div>
+      <Image src="/icons/1.png" alt="Arrow Left" width={48} height={48} />
+      <Image src="/icons/2.png" alt="Arrow Left" width={48} height={48} />
+      <Image src="/icons/3.png" alt="Arrow Left" width={48} height={48} />
+    </div>
+  )
+}
